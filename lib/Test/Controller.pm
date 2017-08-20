19808aa41bdbb1d9b8f48e2201e6934f1cb468fd
root <root@autoxit.ru>
2017-08-20 03:16:24 +0300

    Fixed

--- lib/Test/Controller.pm
+++ lib/Test/Controller.pm
@@ -5,14 +5,14 @@ use parent qw(Test::MVC) ;
 + 1 ;
 
 sub dispatch( $$;@ ) {
-	my ( $self , $data ) = @_ ;
+	my ( $self , $data ) = splice( @_ , 0 , 2 ) ;
 	my $action = $self->{ 'creator' }->action( ) || return +( ) ;
 	my $actions = $self->{ 'dancer' }{ 'config' }{ 'actions' } ;
 
 	return +( ) unless exists( $actions->{ $action } ) ;
 
 	my ( $package , $method ) = $self->__split_package( $actions->{ $action } ) ;
-	my $result = eval { $self->__object( $package , 'data' => $data )->$method( $self , @_ ) } ;
+	my $result = eval { $self->__object( $package , 'data' => $data , @_ )->$method( $self ) } ;
 
 	warn( $@ ) and return +( ) if $@ ;
 
